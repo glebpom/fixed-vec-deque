298216f1bedce55ef6e775519998821d1808624c
Gleb Pomykalov <gleb@lancastr.com>
2020-03-28 17:19:38 +0300

    support creation from preinitialized buffer

--- src/lib.rs
+++ src/lib.rs
@@ -178,6 +178,20 @@ where
         }
     }
 
+    pub fn new_from_array(mut data: T) -> Self {
+        let m = data.ptr_mut();
+
+        for o in 0..T::size() {
+            unsafe { ptr::write(m.add(o), T::Item::default()); }
+        }
+
+        FixedVecDeque {
+            head: 0,
+            len: 0,
+            data,
+        }
+    }
+
     /// Initialize stored data using `Default::default()`
     fn data_from_default() -> T {
         unsafe {
@@ -1410,10 +1424,11 @@ pub unsafe trait Array {
     }
 }
 
+#[macro_export]
 macro_rules! impl_array(
     ($($size:expr),+) => {
         $(
-            unsafe impl<T> Array for [T; $size] where T: Default {
+            unsafe impl<T> $crate::Array for [T; $size] where T: Default {
                 type Item = T;
                 fn size() -> usize { $size }
                 fn ptr(&self) -> *const T { self.as_ptr() }
@@ -1540,7 +1555,7 @@ where
 impl_array!(
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 20, 24, 32, 36, 0x40, 0x80, 0x100,
     0x200, 0x400, 0x800, 0x1000, 0x2000, 0x4000, 0x8000, 0x10000, 0x20000, 0x40000, 0x80000,
-    0x100000
+    0x100000, 0xffffff, 0x2ffffff
 );
 
 /// Returns the two slices that cover the `FixedVecDeque`'s valid range
